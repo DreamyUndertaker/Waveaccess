102302a81c2d05b28f8cab8aed4a298bb9f3b6f1
Dreamy Undertaker <vadiimreg73@gmail.com>
2024-02-20 06:11:14 +0400

    Initial commit

---
+++ src/main/java/ru/DreamyUndertaker/Waveaccess/WaveaccessApplication.java
@@ -0,0 +1,13 @@
+package ru.DreamyUndertaker.Waveaccess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaveaccessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaveaccessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/DreamyUndertaker/Waveaccess/WaveaccessApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.DreamyUndertaker.Waveaccess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaveaccessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
